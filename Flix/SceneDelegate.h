9703a635b708afed0da981e17806478d5c593cdc
Briana Mason <brm14@fb.com>
2020-06-25 12:12:38 -0400

    Initial Commit

---
+++ Flix/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Flix
+//
+//  Created by admin on 6/25/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by admin on 6/25/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
