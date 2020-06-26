cb4a49a4db5ceb79ed84565bf1f3ae03ca392e9f
Briana Mason <brm14@fb.com>
2020-06-26 00:13:03 -0400

    Required User Stories up to 3 complete.

---
+++ Flix/Views/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by admin on 6/25/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END
