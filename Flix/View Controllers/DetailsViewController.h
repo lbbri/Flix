ee5a69751425f51e20908cea42b8898b6ebc89d8
Briana Mason <brm14@fb.com>
2020-06-26 01:58:44 -0400

    Details Page is functional

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by admin on 6/26/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/View Controllers/DetailsViewController.m
@@ -0,0 +1,60 @@
+//
+//  DetailsViewController.m
+//  Flix
+//
+//  Created by admin on 6/26/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface DetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *backdropView;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = self.movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingFormat:posterURLString];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    
+    [self.posterView setImageWithURL:posterURL];
+    
+    
+    NSString *backdropURLString = self.movie[@"backdrop_path"];
+    NSString *fullBackdropURLString = [baseURLString stringByAppendingFormat:backdropURLString];
+    NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
+    
+    [self.backdropView setImageWithURL:backdropURL];
+    
+    self.titleLabel.text = self.movie[@"title"];
+    self.synopsisLabel.text = self.movie[@"overview"];
+    self.dateLabel.text = self.movie[@"release_date"];
+    
+    [self.titleLabel sizeToFit];
+    [self.synopsisLabel sizeToFit];
+    [self.dateLabel sizeToFit];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
